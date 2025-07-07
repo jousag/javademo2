cb03eda22186192fac38971993620715f782cbde
jousag <joose.kukkola@gmail.com>
2025-07-07 17:28:25 +0300

    new file:   src/main/java/viikko1/Animal.class 	new file:   src/main/java/viikko1/Animal.java 	modified:   src/main/java/viikko1/App.java 	new file:   target/classes/viikko1/Animal.class 	modified:   target/classes/viikko1/App.class

--- src/main/java/viikko1/App.java
+++ src/main/java/viikko1/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Animal a = new Animal("Leo", "Lion", 5);
+        System.out.println("Animal Name: " + a.name);
     }
 }
